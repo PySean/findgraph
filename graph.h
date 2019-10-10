ffcd1cbbead349b2e2ddc487694ea9ab03b081d3
PySean <smsod18@hotmail.com>
2019-10-10 11:57:33 -0500

    Added makefile and tentative graph header.

---
+++ graph.h
@@ -0,0 +1,39 @@
+#include "stack.h"
+
+
+typedef struct graph {
+    //The adjacency matrix for the graph.
+    bool ** adj_mat;
+    int len;
+    int width;
+    //Signifies whether we have edges other than the boundary of the vg.
+    bool is_base; 
+} Graph;
+
+typedef struct graphlist {
+    struct graphlist * prev;
+    Graph * g;
+    struct graphlist * next;
+} GraphList;
+
+//Allocates & initializes a basic terrain vg with num_vertices.
+Graph * makeGraph(int num_vertices);
+
+//Appends GraphList b to GraphList a.
+GraphList * append (GraphList * a, GraphList * b);
+
+//Generates the vg corresponding to stack s.
+Graph * fromStack(Stack * s);
+
+//Creates a set of new graphs by transposing previously computed graphs over
+//the currently computed graph g.
+GraphList * makeGraphs(Graph * g, GraphList * prevs);
+
+//Checks if graph g is a legal visibility graph by checking order claim and
+//bar property.
+bool isLegal(Graph * g);
+
+//Operations for deleting a single graph or a list of them.
+void deleteGraph(Graph * g);
+
+void deleteGraphs(GraphList * gl);
---
+++ makefile
@@ -0,0 +1,19 @@
+objects = main.o findgraph.o stack.o graph.o
+
+run: $(objects)
+	gcc -o run $(objects)
+
+main.o: main.c
+	gcc -c main.c
+
+findgraph.o: findgraph.c
+	gcc -c findgraph.c
+
+stack.o: stack.c
+	gcc -c stack.c
+
+graph.o: graph.c
+	gcc -c graph.c
+
+clean:
+	rm $(objects) run
