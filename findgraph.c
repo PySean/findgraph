104827f256413889328275826d23ce3b19d46d31
PySean <smsod18@hotmail.com>
2019-10-31 03:25:16 -0500

    Restructured outer loop structure. Still working out the kinks.

--- findgraph.c
+++ findgraph.c
@@ -2,54 +2,59 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include "stack.h"
+#include "graph.h"
 #include "findgraph.h"
+//Since there's only two possibilities for graphs with 3 vertices, we begin
+//our calculations with 4.
+#define MIN_VERTICES 4
 
 /**
     "findgraph.c", by Sean Soderman
-    Brute forces visibility graphs that satisfy order claim and cordless 
-    cycle.
+    Determines all possible visibility graphs.
 */
-void findgraphs(int max_vertices) {
+void findgraphs(int max_vertices, char * filename) {
+    //First: Initialize an initial graphlist of graphs with n = 3.
+    GraphList * prevs = makeGraphList();
+    Graph * init = makeGraph(3, max_vertices);
+    append(graphCopy(init), prevs);
+    init->adj_mat[0][2] = true;
+    init->adj_mat[2][0] = true;
+    append(init, prevs);
+    //Newly generated graphs.
+    GraphList * news = makeGraphList();
     int i = 0;
-    int combination = 0; //This is a test to see if my stack algorithm is correct.
-    int verts_to_check = max_vertices - 2; //Final two vertices aren't checked.
-    for (i = 2; i <= verts_to_check; i++) {
-        //We don't include the final two vertices, as no edges are drawn between
-        //them and the final vertex.
-        Stack * s = malloc(sizeof(Stack));
-        memset(s, 0, sizeof(Stack));
-        bool popped = false;
-        do {
-            if (popped) {
-                if (top(s) == 0) {
-                    setTop(s, true);
-                    popped = false; //Go deeper down the stack again.
-                }
-                else {
-                    s = pop(s);
-                    popped = true;
-                }
-            }
-            else {
-                s = push(s, false);
-                if (s->n > i) {
-                    //fprintf(stderr, "does this happen\n");
-                    combination++;
-                    s = pop(s);
-                    popped = true;
-                    printStack(s);
-                    //Graph ** g = makeGraphs(s, prevGraphs);
-                    //currGraphs.add(g);
-                }
-            }
-        } while (s->n != 0);
-        /*
-        prevGraphs.delete();
-        prevGraphs = currGraphs;
-        writeToFile(currGraphs);
-        */
-        free(s);
+    for (i = MIN_VERTICES; i <= max_vertices; i++) {
+        Graph * prev = NULL;
+        GraphList * curr = NULL;
+        for (curr = prevs; curr != NULL; curr = prevs->next) {
+            prev = curr->g;
+            //Extend the previously computed graph by a vertex and edge.
+            prev->adj_mat[prev->len][prev->len + 1] = true;
+            prev->adj_mat[prev->len + 1][prev->len] = true;
+            prev->len++;
+            //We begin at the third vertex from the right, since we
+            //only care about adding new edges to vertex n.
+            graph_gen(prev, news, i - 2);
+        }
+        //NOTE: Might need a threshold for writing graphs if they occupy too much
+        //memory.
+        writeGraphs(news, filename);
+        deleteGraphs(prevs);
+        GraphList * temp = news;
+        news = prevs;
+        prevs = temp;
     }
-    printf("The number of combinations is %d\n", combination);
+}
+
+/**
+    From a list of graphs with n-1 vertices in "prevs", generates a new
+    list of graphs with n+1 vertices.
+*/
+void graph_gen(Graph * prev, GraphList * news, int vert) {
+    if (vert < 0)
+        return;
+    //First. Fix the graph, if possible. If not possible, we return.
+    //Path 1: edge (vert, n) is picked, we check if it is OK.
+
+
 }
